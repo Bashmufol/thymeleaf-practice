0f9cc890b538abea8e92db04f792b489874d3634
Bashir Muhammed <bashmufol@gmail.com>
2025-06-13 11:22:03 +0100

    Adding @NoArgsContructor to the user class

--- src/main/java/com/bash/thymeleaftutorial/model/User.java
+++ src/main/java/com/bash/thymeleaftutorial/model/User.java
@@ -2,11 +2,13 @@ package com.bash.thymeleaftutorial.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class User {
     private String name;
     private String email;
